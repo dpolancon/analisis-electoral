b0db54f241915586262a7234bb57d5797d9f4d87
dpolancon <dpolancon@gmail.com>
2023-12-17 21:55:54 -0300

    Repo Analisis Electoral - IPSOL
    Repositorio para el desarrollo de analisis electoral

---
+++ limpieza_bbdd_madre.R
@@ -0,0 +1,42 @@
+
+##### preambulo para abrir la base de datos ####   
+
+path <- "Locales_Plebiscito_Salida_Dic_2023_con_Analisis_de_Prioridad.xlsx" # path al archivo
+data_master <- read_excel(path) #objeto de la base master 
+
+
+View(data_master)
+
+
+colnames(data_master)
+
+
+"region",
+"region_nombre",
+"comuna",
+"local_de_votacion",
+"direccion_local_votacion",
+"mesas_Dic2022",
+"mesas_Mayo2023", 
+
+
+
+
+
+"id_mayo"
+"x_latitud"
+"y_longitud"                       
+"partido_republicano"
+"partido_delagente"
+"pacto_derecha"                 
+"pactO_PPD+PR+PDC"                 
+"Pacto FA+PC+PS"                  
+"Indep."                           
+"Votos Nulos...17"                 
+"Oficialismo"                     
+"Apruebo"
+"Rechazo"                       
+"Votos Nulos...21"                
+"Evaluacion1 (Apruebo/ CC)"
+"Evaluacion2 (aumento de nulos)"   
+"Evaluacion3 (aumento la Derecha)"
